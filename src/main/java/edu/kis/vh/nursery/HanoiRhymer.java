622af3a1c148b315b66603f0aaedcfde32c42493
Weronika Marciniak <weronika_marciniak@interia.pl>
2021-01-24 18:29:10 +0100

    Editing class fields names

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,17 +1,17 @@
 package edu.kis.vh.nursery;
 
-public class HanoiRhymer extends defaultCountingOutRhymer {
+public class HanoiRhymer extends DefaultCountingOutRhymer {
 
 	int totalRejected = 0;
 
 	public int reportRejected() {
 		return totalRejected; // wrongly formated
-	} 
+	}
 
 	public void countIn(int in) {
 		if (!callCheck() && in > peekaboo()) // wrongly formated
-			totalRejected++;     // wrongly formated
-		else                    // wrongly formated
-			super.countIn(in);  // wrongly formated
+			totalRejected++; // wrongly formated
+		else // wrongly formated
+			super.countIn(in); // wrongly formated
 	}
 }
