797895117050edbcb2d1d9aec639c762297125df
J.Przydalski <przydal@gmail.com>
2021-01-30 17:12:40 +0100

    add constants, add override

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -8,6 +8,7 @@ public class HanoiRhymer extends DefaultCountingOutRhymer {
 		return totalRejected; // wrongly formated
 	}
 
+	@Override
 	public void countIn(int in) {
 		if (!callCheck() && in > peekaboo()) // wrongly formated
 			totalRejected++; // wrongly formated
