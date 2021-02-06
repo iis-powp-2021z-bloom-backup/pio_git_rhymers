b11a4f7c1c90dcb7ea65df5b86059875fefb2e53
Pawel_W <kontakt@pawelwozniak.pl>
2021-02-06 18:33:07 +0100

    1.4 Change variables to const

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -2,6 +2,8 @@ package edu.kis.vh.nursery.list;
 
 public class IntLinkedList {
 
+	private static final int EMPTY_INDICATOR = -1;
+
 	Node last;
 	int i;
 
@@ -25,13 +27,13 @@ public class IntLinkedList {
 
 	public int top() {
 		if (isEmpty())
-			return -1;
+			return EMPTY_INDICATOR;
 		return last.value;
 	}
 
 	public int pop() {
 		if (isEmpty())
-			return -1;
+			return EMPTY_INDICATOR;
 		int ret = last.value;
 		last = last.prev;
 		return ret;
